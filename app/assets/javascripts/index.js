aa4168d1da8aa74a6158a22e58ed657d418aee3d
Jessica <jmc617@gmail.com>
2018-06-18 17:52:51 -0400

    api data pull functional

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,3 +1,16 @@
 $(document).on("turbolinks:load", function(){
-
+  $.ajax({
+  type:"GET",
+  url:"https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=oUPwzHwcM1P78UtYeEbbaHZAxcIU93DC",
+  async:true,
+  dataType: "json",
+  success: function(json) {
+              console.log(json);
+              // Parse the response.
+              // Do other things.
+           },
+  error: function(xhr, status, err) {
+              // This time, we do not end up here!
+           }
+});
 })
