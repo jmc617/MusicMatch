9d89814e6b23be5917626b5c10e3b34d981a223e
ChristianAJohnson <chrisgochris@gmail.com>
2018-06-21 17:29:54 -0400

    chat windows now shows avatar pictures

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -11,6 +11,6 @@ class RoomChannel < ApplicationCable::Channel
   	# ActionCable.server.broadcast 'room_channel', data['message'] 
   	# The above command was used to broadcast the message on to the screen and test that Action Cable was working.
   	# This broadcast is now being sent using the broadcast_message_job (app/jobs/broadcast_message_job.rb)
-  	Message.create content: data["message"]
+  	Message.create content: data["message"], user: current_user
   end
 end
