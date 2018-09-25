1bd3462ed2da514264b39310b09ab4236df2c495
Jessica <jmc617@gmail.com>
2018-09-24 20:54:00 -0400

    guest user classes added to login fields

--- app/assets/javascripts/guestuser.js
+++ app/assets/javascripts/guestuser.js
@@ -1,3 +1,10 @@
 $(document).on("turbolinks:load", function(){
-    
+    var emailField = document.getElementsByClassName('email-field');
+    var passwordField = document.getElementsByClassName('password-field');
+    var guestUserBtn = document.getElementsByClassName('guest-user-btn');
+
+    guestUserBtn[0].addEventListener("click", function(){
+        emailField[0].value = 'testuser@gmail.com'
+        passwordField[0].value =  'jessica' 
+    })
 })
